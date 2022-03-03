91acb9117f19d82063dbc480c0dd0bf30d3226b5
Beth <bethmelmtv@gmail.com>
2022-03-02 16:31:54 -0800

    added if else statement to event listener

--- app.js
+++ app.js
@@ -28,14 +28,27 @@ console.log(pronouns);
     // get user input
 selectElement.addEventListener('change', (e) => {
     console.log('user click');
+    const selected = e.target.value;
+    hobbies.innerHTML = ' ';
+    divContainer.classList.remove('Beth', 'Susan');
+
+    if (selected === 'one') {
+        renderPerson(0);
+    } else {
+        renderPerson(1);
+    }
 });
+
+
+
+
     // use user input to update state 
 function renderPerson(index) {
         //inside here will do something to the dom
     divContainer.classList.add(`${people[index].name}`);
     name.textContent = 'Name: ' + people[index].name;
     pronouns.textContent = 'Pronouns: ' + people[index].pronouns;
-    favPlace.textContent = 'Favorite Place: ' + people[index].fav-place;
+    favPlace.textContent = 'Favorite Place: ' + people[index].favorite_place;
 
     for (let hobby of people[index].hobbies) {
         const li = document.createElement('li');
@@ -43,5 +56,7 @@ function renderPerson(index) {
         hobbies.appendChild(li);
     }
 }
+
+renderPerson(0);
 
     // update DOM to reflect the new state
